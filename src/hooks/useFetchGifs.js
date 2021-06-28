372dd2890aa0d77b6d2da5d2d9c4c1eeb0ca8622
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-28 11:44:27 -0400

    Creado custom hook para organizar la logica

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/giphyCalls";
 
-export const useFetchGifs = () => {
-  const [state, setstate] = useState({
+//
+export const useFetchGifs = (category) => {
+  const [state, setState] = useState({
     data: [],
     loading: true,
   });
+
+  // Si no le añadimos dependencias al useEffect (es decir, el ultimo argumento como '[]')
+  // solo se ejecutara el efecto una sola vez. Cuando se crea el componente.
+  // Los efectos no pueden ser asincronos.
+  useEffect(() => {
+    getGifs(category).then((gifs) =>
+      setState({
+        data: gifs,
+        loading: false,
+      })
+    );
+  }, [category]);
 
   return state;
 };
